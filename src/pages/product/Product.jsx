9b11fdf9045d30ff03e5ff3842d09770d66c5cc4
niloofarizadyar <niloofarizadyar1375@gmail.com>
2023-05-22 00:48:11 +0430

    product was created

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Storage from './../../assets/storage.jpg'
+import './product.css'
 
 const Product = () => {
+  const [count, setCount] = useState(1)
   return (
-    <div>Product</div>
+    <div className='product'>
+      <div className='product-img-container'>
+        <img className='product-img' src={Storage} />
+        
+      </div>
+      <div className='product-description'>
+        <h2 className='single-product-info'>My Title</h2>
+        <h3 className='single-product-info'>Price : 100$</h3>
+        <p className='single-product-info'>this is a description.</p>
+        <div className='product-add-count'>
+          <button className='count-btn' onClick={()=>(setCount((prevState)=>prevState == 1 ? 1 : prevState -1))}>-</button>
+          <p className='count-num'>{count}</p>
+          <button className='count-btn' onClick={()=>(setCount((prevState)=>prevState + 1))}>+</button>
+        </div>
+        <button className='add-to-card-btn'>ADD TO CARD</button>
+      </div>
+    </div>
   )
 }
 
